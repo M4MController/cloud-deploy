992e458e359c611aac9682ac5d93bdc09781b43d
Ed Asriyan <ed-asriyan@protonmail.com>
2020-05-13 21:42:52 +0300

    Pull from M4M registry

---
+++ update-registry.sh
@@ -0,0 +1,19 @@
+# cloud-database
+docker build -t registry.meter4.me:25410/cloud-database -f Dockerfile-db https://github.com/M4MController/cloud-web.git
+docker push registry.meter4.me:25410/cloud-database
+
+# cloud-backend
+docker build -t registry.meter4.me:25410/cloud-backend https://github.com/M4MController/cloud-web.git
+docker push registry.meter4.me:25410/cloud-backend
+
+# cloud-frontend
+docker build -t registry.meter4.me:25410/cloud-frontend --build-arg MODE=default https://github.com/M4MController/frontend.git
+docker push registry.meter4.me:25410/cloud-frontend
+
+# cloud-frontend-admin
+docker build -t registry.meter4.me:25410/cloud-frontend-admin https://github.com/M4MController/frontend-admin.git
+docker push registry.meter4.me:25410/cloud-frontend-admin
+
+# cloud-sync
+docker build -t registry.meter4.me:25410/cloud-sync -f sync/Dockerfile https://github.com/M4MController/cloud-web.git
+docker push registry.meter4.me:25410/cloud-sync
